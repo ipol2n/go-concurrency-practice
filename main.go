84f70a7bd49081e00bf75bc1487dfc60481c43e7
Kittiporn Korprasertthaworn <izporn@gmail.com>
2018-07-30 22:01:35 +0700

    Refactor code: queue things

--- main.go
+++ main.go
@@ -41,19 +41,49 @@ func fetchAPI(url string, c chan Result) {
 	c <- result
 }
 
-func createQueue()
-
 func fetch(postID string, c chan Result) {
 	fetchC := make(chan Result)
-	go fetchAPI("http://192.168.1.54:3000/posts/"+postID, fetchC)
-	go fetchAPI("http://192.168.1.54:3001/posts/"+postID, fetchC)
+	go fetchAPI("http://127.0.0.1:3000/posts/"+postID, fetchC)
+	go fetchAPI("http://127.0.0.1:3001/posts/"+postID, fetchC)
 	result := <-fetchC
 	c <- result
 }
 
+func createQueue(ch *amqp.Channel, queueName string) amqp.Queue {
+	q, err := ch.QueueDeclare(
+		queueName, // name
+		true,      // durable
+		false,     // delete when unused
+		false,     // exclusive
+		false,     // no-wait
+		nil,       // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+	err = ch.Qos(
+		1,     // prefetch count
+		0,     // prefetch size
+		false, // global
+	)
+	failOnError(err, "Failed to set QoS")
+	return q
+}
+
+func consumeQueue(ch *amqp.Channel, queueName string) <-chan amqp.Delivery {
+	msgs, err := ch.Consume(
+		queueName, // queue
+		"",        // consumer
+		false,     // auto-ack
+		false,     // exclusive
+		false,     // no-local
+		false,     // no-wait
+		nil,       // args
+	)
+	failOnError(err, "Failed to register a consumer")
+	return msgs
+}
+
 func main() {
 	conn, err := amqp.Dial("amqp://root:root@127.0.0.1:5672/")
-	fmt.Println(conn.type())
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
@@ -61,71 +91,12 @@ func main() {
 	failOnError(err, "Failed to open a channel")
 	defer ch.Close()
 
-	ch2, err := conn.Channel()
-	failOnError(err, "Failed to open a channel")
-	defer ch2.Close()
+	q := createQueue(ch, "input")
+	q2 := createQueue(ch, "base")
+	q3 := createQueue(ch, "result")
 
-	q, err := ch.QueueDeclare(
-		"input", // name
-		true,    // durable
-		false,   // delete when unused
-		false,   // exclusive
-		false,   // no-wait
-		nil,     // arguments
-	)
-	failOnError(err, "Failed to declare a queue")
-	q2, err := ch2.QueueDeclare(
-		"base", // name
-		true,   // durable
-		false,  // delete when unused
-		false,  // exclusive
-		false,  // no-wait
-		nil,    // arguments
-	)
-	failOnError(err, "Failed to declare a queue")
-	q3, err := ch.QueueDeclare(
-		"result", // name
-		true,     // durable
-		false,    // delete when unused
-		false,    // exclusive
-		false,    // no-wait
-		nil,      // arguments
-	)
-	failOnError(err, "Failed to declare a queue")
-
-	err = ch.Qos(
-		1,     // prefetch count
-		0,     // prefetch size
-		false, // global
-	)
-	failOnError(err, "Failed to set QoS")
-	err = ch2.Qos(
-		1,     // prefetch count
-		0,     // prefetch size
-		false, // global
-	)
-	failOnError(err, "Failed to set QoS")
-
-	msgs, err := ch.Consume(
-		q.Name, // queue
-		"",     // consumer
-		false,  // auto-ack
-		false,  // exclusive
-		false,  // no-local
-		false,  // no-wait
-		nil,    // args
-	)
-	failOnError(err, "Failed to register a consumer")
-	msgs2, err := ch2.Consume(
-		q2.Name, // queue
-		"",      // consumer
-		false,   // auto-ack
-		false,   // exclusive
-		false,   // no-local
-		false,   // no-wait
-		nil,     // args
-	)
-	failOnError(err, "Failed to register a consumer")
+	inputConsumer := consumeQueue(ch, q.Name)
+	baseConsumer := consumeQueue(ch, q2.Name)
 
 	forever := make(chan bool)
 	base := 0
@@ -133,7 +104,7 @@ func main() {
 	go func() {
 		for {
 			select {
-			case d := <-msgs:
+			case d := <-inputConsumer:
 				body := string(d.Body)
 				numbers := strings.Split(body, ",")
 				for _, v := range numbers {
@@ -141,7 +112,6 @@ func main() {
 					go fetch(fmt.Sprintf("%d", n+base), c)
 				}
 				for i := 0; i < len(numbers); i++ {
-					// fmt.Printf("%+v\n", <-c)
 					s, _ := json.Marshal(<-c)
 					fmt.Println(string(s))
 					err = ch.Publish(
@@ -156,8 +126,9 @@ func main() {
 						})
 				}
 				d.Ack(false)
-			case d := <-msgs2:
+			case d := <-baseConsumer:
 				base, _ = strconv.Atoi(string(d.Body))
+				fmt.Printf("Got new base: %d\n", base)
 				d.Ack(false)
 			}
 		}
